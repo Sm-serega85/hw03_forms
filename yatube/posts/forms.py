99d6d03569ab7cca3bc9da12661013d1ac740ca1
Сергей Смирнов <sm-serega85@yandex.ru>
2022-12-27 22:58:04 +0300

    sprint 4 20221227_

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -4,9 +4,8 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
-    text = forms.CharField(
-            label=Post._meta.get_field("text").verbose_name,
-            help_text=Post._meta.get_field("text").help_text)
+    text = forms.CharField(label=Post._meta.get_field("text").verbose_name,
+                           help_text=Post._meta.get_field("text").help_text)
 
     class Meta:
         model = Post
